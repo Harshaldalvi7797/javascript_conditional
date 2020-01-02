eb8bbc7a064ced5aaeb87dbe28533f2ea2e421af
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-01-02 18:14:08 +0530

    #harshal :new project

---
+++ main.js
@@ -0,0 +1,63 @@
+let numValue1=10;
+let numValue2 =12;
+
+if(numValue1==numValue2)
+{
+    console.log("Both Numbers are same.");
+}
+
+else{
+    console.log("Both Numbers are different");
+}
+//ternary operator
+
+let message = undefined;
+
+message=(numValue1==numValue2)
+? "both numbers are same"
+: "Both Numbers are different";
+
+console.log(message);
+
+let strvalue = "10";
+let numValue3 =10;
+
+if(strvalue===numValue3)
+{
+    console.log("Both Numbers are same.");
+
+}
+else{
+    console.log("Both Numbers are different");
+
+}
+
+let numValue6 =9;//Number
+let numValue7 =8; //Number
+
+if(numValue6 != numValue7)
+{
+    console.log("True");
+}
+
+else{
+    console.log("False");
+}
+
+let strFirstname= "Harshal";
+let strLastName = "Dalvi";
+if(strFirstname < strLastName)
+{
+    console.log("true part");
+
+}
+
+else{
+    console.log("False Part");
+}
+
+
+
+
+
+
